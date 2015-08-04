3348a19860b255a55dc4f22840e1797474715690
Caroline Appleby <caroline.appleby@cantab.net>
2015-08-04 17:41:02 +0100

    Slightly prettier GUI, now using grid() not pack()

--- enigmaGUIbetter.py
+++ enigmaGUIbetter.py
@@ -1,17 +1,18 @@
 # TODO error handling!!! find out how to pop up an error message
+# TODO make pretty
 
 from tkinter import *
 from tkinter import ttk
 import enigma
 
 root1 = Tk()  # root window
-root1.title("Better Encryptor!")
+root1.title("Enigma!")
 
 
 # make an Entry to take plaintext input, label it
 plaintext_entry_label = ttk.Labelframe(root1, text="Input plaintext:")
 plaintext_entry = Text(plaintext_entry_label, height=4, width=50)
-plaintext_entry.pack()
+plaintext_entry.grid(padx=3, pady=5)
 
 
 # entry for displaying ciphertext output
@@ -19,63 +20,62 @@ ciphertext_entry_label = ttk.Labelframe(root1, text="Your ciphertext:")
 ciphertext = ""
 ciphertext_entry = Text(ciphertext_entry_label, height=4, width=50)
 ciphertext_entry.insert(END, ciphertext)
-ciphertext_entry.pack()
+ciphertext_entry.grid(padx=3, pady=5)
 
 
 settings_frame = ttk.Labelframe(root1, text="Settings")
 # dropdowns to select which scrambler to use
 available_rotors = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'none']
 
-pick1 = ttk.Labelframe(settings_frame, text='Select first rotor')
-pick1.pack()
-select_rotor_1 = ttk.Combobox(pick1, values=available_rotors, state='readonly')
+
+
+
+pick = ttk.Labelframe(settings_frame, text='Select rotors')
+pick.grid(column=0, row=0, pady=5)
+select_rotor_1 = ttk.Combobox(pick, values=available_rotors, state='readonly', width=5)
 select_rotor_1.current(0)  # set which one appears by default
-select_rotor_1.pack()
+select_rotor_1.grid(column=0, row=0, padx=3, pady=5)
 
-pick2 = ttk.Labelframe(settings_frame, text='Select second rotor')
-pick2.pack()
-select_rotor_2 = ttk.Combobox(pick2, values=available_rotors, state='readonly')
+select_rotor_2 = ttk.Combobox(pick, values=available_rotors, state='readonly', width=5)
 select_rotor_2.current(8)
-select_rotor_2.pack()
+select_rotor_2.grid(column=1, row=0, padx=3, pady=5)
 
-pick3 = ttk.Labelframe(settings_frame, text='Select third rotor')
-pick3.pack()
-select_rotor_3 = ttk.Combobox(pick3, values=available_rotors, state='readonly')
+select_rotor_3 = ttk.Combobox(pick, values=available_rotors, state='readonly', width=5)
 select_rotor_3.current(8)
-select_rotor_3.pack()
+select_rotor_3.grid(column=2, row=0, padx=3, pady=5)
 
 
 # dropdown to select which reflector to use
 available_reflectors = ["A", "B", "C", "B thin", "C thin"]
 pick4 = ttk.Labelframe(settings_frame, text='Select reflector')
-pick4.pack()
+pick4.grid(column=0, row=2, pady=5)
 select_reflector = ttk.Combobox(pick4, values=available_reflectors, state='readonly')
 select_reflector.current(0)
-select_reflector.pack()
+select_reflector.grid(padx=3, pady=5)
 
 
 # Entries to specify starting positions of rotors
 set_pos = ttk.Labelframe(settings_frame, text='Set starting positions')
-set_pos.pack()
+set_pos.grid(row=1, pady=5)
 pos1 = ''
-set1 = ttk.Entry(set_pos, textvariable=pos1)
-set1.pack()
+set1 = ttk.Entry(set_pos, textvariable=pos1, width=5)
+set1.grid(column=0, row=1, padx=10, pady=5)
 pos2 = ''
-set2 = ttk.Entry(set_pos, textvariable=pos2)
-set2.pack()
+set2 = ttk.Entry(set_pos, textvariable=pos2, width=5)
+set2.grid(column=1, row=1, padx=10, pady=5)
 pos3 = ''
-set3 = ttk.Entry(set_pos, textvariable=pos3)
-set3.pack()
+set3 = ttk.Entry(set_pos, textvariable=pos3, width=5)
+set3.grid(column=2, row=1, padx=10, pady=5)
 
 
 # entry to add plugboard settings
 plugboard_entry_label = ttk.Labelframe(settings_frame, text="Set plugboard, eg a:z b:q")
-plugboard_entry_label.pack()
+plugboard_entry_label.grid(row=3, pady=5)
 plugtext = ""
 plugboard_entry_text = StringVar()
 plugboard_entry_text.set(plugtext)
 plugboard_entry = ttk.Entry(plugboard_entry_label, textvariable=plugboard_entry_text)
-plugboard_entry.pack()
+plugboard_entry.grid(padx=3, pady=2)
 
 
 # bind encrypt button to function displaying ciphertext
@@ -100,8 +100,6 @@ def encrypt():
     # define the machine to be used for encryption
     current_machine = enigma.Machine(scram_list, refl, plug)
 
-
-
     # do the encryption
     ciphertext = current_machine.encrypt(plaintext_entry.get(1.0, END))
     ciphertext_entry.insert(END, ciphertext)
@@ -118,12 +116,11 @@ plaintext_entry.focus()
 
 
 # PACK ALL THE THINGS!
-# TODO (eventually!) make look pretty
-plaintext_entry_label.pack()
-settings_frame.pack()
-encrypt_button.pack()
-ciphertext_entry_label.pack()
-close_button.pack(side=BOTTOM)
+plaintext_entry_label.grid(column=0, row=0, pady=10, padx=10)
+settings_frame.grid(column=1, row=0, rowspan=3, padx=10, pady=10, ipadx=10, ipady=10)
+encrypt_button.grid(column=0, row=1, pady=10)
+ciphertext_entry_label.grid(column=0, row=2, pady=10, padx=10)
+close_button.grid(column=1, row=3, pady=10)
 
 
 root1.mainloop()
